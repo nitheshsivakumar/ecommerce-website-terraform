5d82f243b53b905d882e3bbcff96a35975eaf108
nitheshsivakumar <nitheshsivakumar19@gmail.com>
2025-02-23 15:22:18 -0600

    added route-53

--- variable.tf
+++ variable.tf
@@ -121,3 +121,16 @@ variable "ec2_key_pair_name" {
     description = "ec2 keypair name"
     type        = string
 }
+
+# route 53 variables
+variable "domain_name" {
+    default     = "nitheshskmr.com"
+    description = "domain name"
+    type        = string
+}
+
+variable "record_name" {
+    default     = "www"
+    description = "sub domain name"
+    type        = string
+}
